0fe8021f00f38d906dcb88ff30ed6e3f64c518ac
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2024-09-25 12:15:08 +0700

    update

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,9 @@
 echo "BUILD START"
 apt-get update
-apt-get install -y python3 python3-pip
+apt-get install -y python3 python3-pip python3-venv
 python3.12 -m venv venv
 source venv/bin/activate
+pt-get install -y python3 python3-pip python3-venv
 pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput
 echo "BUILD END"
