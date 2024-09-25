39c4929e16a5a2893ec97910fc2cc8b1d6a76fbb
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2024-09-25 12:21:43 +0700

    update

--- build_files.sh
+++ build_files.sh
@@ -6,5 +6,4 @@ python3.12 -m venv venv
 source venv/bin/activate
 pt-get install -y python3 python3-pip
 pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput
 echo "BUILD END"
