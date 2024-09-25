d86210ac8eea42167f8ee7a4a67ccc744cefc333
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2024-09-25 12:18:11 +0700

    update

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,10 @@
 echo "BUILD START"
 apt-get update
 apt-get install -y python3 python3-pip python3-venv
+apt-get install -y libpq-dev
 python3.12 -m venv venv
 source venv/bin/activate
-pt-get install -y python3 python3-pip python3-venv
+pt-get install -y python3 python3-pip
 pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput
 echo "BUILD END"
